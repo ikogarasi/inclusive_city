2f4f92392a2452c77721069ed5a3f950d300f32d
Vitalii Kutsan <vkutsan.bs@gmail.com>
2023-12-17 18:41:24 +0200

    fixed question

--- CD/Incity/Incity.Services.AuthAPI/Infrastructure/AuthDbContext.cs
+++ CD/Incity/Incity.Services.AuthAPI/Infrastructure/AuthDbContext.cs
@@ -7,19 +7,13 @@ namespace Incity.Services.AuthAPI.Infrastructure
     public class AuthDbContext : IdentityDbContext<IncityUser, IncityRole, string>
     {
         private readonly IConfiguration _configuration;
-        private static bool _ensureCreated = false;
-
+        
         public AuthDbContext(DbContextOptions<AuthDbContext> options, IConfiguration configuration)
             : base(options)
         {
             _configuration = configuration;
 
-            if (!_ensureCreated)
-            {
-                Database.EnsureDeleted();
-                Database.EnsureCreated();
-                _ensureCreated = true;
-            }
+            Database.EnsureCreated();
         }
 
         protected override void OnModelCreating(ModelBuilder builder)
